011526b2fcc7b33109caf1e5e50eb23de6ffd9cc
Ted Polley <tedpolley@134-68-180-164.dhcp-in.iupui.edu>
2018-07-13 15:10:39 -0400

    Started work on getting plagiarism policy, APC info, and URL from DOAJ

--- journal-quality.R
+++ journal-quality.R
@@ -1,17 +1,16 @@
-## Look up journals in SHERPA/RoMEO API
+## Look up journals in SHERPA/RoMEO and DOAJ APIs
+
 # install.packages("dplyr")
 # install.packages("httr")
+# install.packages("jsonlite")
 # install.packages("magrittr")
 # install.packages("XML")
 
 # Set working directory
-setwd("~/Desktop/journal-quality")
+setwd("~/Desktop/journal-quality-study")
 
 # Load data exported from Digital Measures w/ journal titles formatted as API query strings
 dm.data <- read.csv("sample-data.csv", strip.white = TRUE, stringsAsFactors = FALSE, na.strings = c("","NA"))
-
-# Save journal titles formatted as query strings as a vector, remove duplicates
-query.vector <- c(dm.data$journal_query) %>% unique()
 
 # initialize API URL
 url <- "http://www.sherpa.ac.uk/romeo/api29.php"
@@ -24,14 +23,36 @@ sherpa.search <- function(x){
   return(journal)
 }
 
+# Create and deduplicate a vector that holds SHERPA query strings
+query.vector <- c(dm.data$journal_query) %>% unique()
+
 # Apply sherpa.search() to list of journal titles (formatted as API query strings)
 sherpa.results <- lapply(query.vector, sherpa.search)
 
 # Combine list of dataframes into one dataframe
 sherpa.results <- do.call("rbind", sherpa.results)
 
-# Rename the columns of sherpa.results dataframe
+# Rename the columns of sherpa.results dataframe to facilite a join
 colnames(sherpa.results) <- c("journal_controlled", "issn", "zetoc_pub", "romeo_pub")
 
 # Combine dm.data and sherpa.results based on journal_controlled
 data <- left_join(dm.data,sherpa.results, by = "journal_controlled")
+
+# Copy the ISSN from Digital Measures data into the data$issn column, if none are found in SHERPA
+data$issn <- ifelse(is.na(data$issn), data$issn_dm, as.character(data$issn))
+
+# Create new column in dataframe that will hold DOAJ query string
+data$doaj_query <- ifelse(is.na(data$issn), data$issn, as.character(paste0("https://doaj.org/api/v1/search/journals/", data$issn)))
+
+# DOAJ Search function
+doaj.search <- function(x){
+  doaj.search <- GET(x)
+  parsed.content <- content(doaj.search, as = "text", encoding = "UTF-8")
+  results <- fromJSON(parsed.content)
+  journal <- results[[4]] %>% as.data.frame()
+  return(journal)
+}
+
+# Create a vector that holds DOAJ query strings
+query.vector <- c(data$doaj_query)
+doaj.results <- lapply(query.vector, doaj.search)
